46930d13fe1d451a4efc63469e0dfb6e9665c481
dore mioto <dore.mioto@gmail.com>
2015-12-03 16:08:13 +0200

    Removed useless data

--- new.project/src/main/webapp/s/js/src/components/base-directive/base-dir.js
+++ new.project/src/main/webapp/s/js/src/components/base-directive/base-dir.js
@@ -1,21 +1,12 @@
 $AppUtils.directive({
 	name: 'baseDirective',
 	templateUrl: './s/js/src/components/base-directive/base-dir.html',
-	controller: function (scope, greetingsService, http) {
+	controller: function (scope) {
 
-		this.someAbout = function(){
-			console.log(scope.injectArgs.b);
-			greetingsService.alarm();
-		};
 	},
 	dependencies: {
 		services: [{
 			name: '$scope'
-		}, {
-			url: 'src/components/base-directive/service/greetings-service'
-		}, {
-			name: '$http'
-		}],
-		directives: ['src/components/header/site-header']
+		}]
 	}
 });
--- new.project/src/main/webapp/s/js/src/components/header/site-header.js
+++ new.project/src/main/webapp/s/js/src/components/header/site-header.js
@@ -1,20 +1,12 @@
 $AppUtils.directive({
 	name : 'siteHeader',
 	templateUrl : './s/js/src/components/header/site-header.html',
-	controller : function(scope, greetingsService, http) {
+	controller : function(scope) {
 
-		this.someAbout = function() {
-			greetingsService.alarm();
-		};
 	},
-	dependencies : {
-		services : [ {
-			name : '$scope'
-		}, {
-			url : 'src/components/base-directive/service/greetings-service'
-		}, {
-			name : '$http'
-		}],
-		directives : [ 'src/components/header/menu/menu' ]
+	dependencies: {
+		services: [{
+			name: '$scope'
+		}]
 	}
 });
